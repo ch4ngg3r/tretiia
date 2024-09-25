56a60e6af3ad79db514bc3212b2972f464268c47
Денис Миниязов < @42-10>
2024-09-25 09:37:18 +0500

    Добавьте файлы проекта.

---
+++ pr03-master/pr03/MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pr03
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private static List<user> users = new List<user>();
+        private static int[] inde = new int[5];
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void Load() 
+        {
+            int v = 1;
+            users.Add(new user
+            {
+                id = Convert.ToInt32(tx.Text),
+                name = ta.Text,
+                password = tb.Text,
+            });
+            inde[v] = Convert.ToInt32(tx.Text);
+
+            v += 1;
+        }
+
+        public class user 
+        { 
+            public int id { get; set; }
+            public string name { get; set; }
+            public string password { get; set; }
+        }
+
+        private void ba_Click(object sender, RoutedEventArgs e)
+        {
+            Load();
+            foreach (var item in users) 
+            { 
+                lia.Items.Add(item);
+            }
+        }
+       
+
+
+        private void ta_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+
+
+        public static void Quicksort(int[] elements, int left, int right)
+        {
+            int i = left;
+            int j = right;
+
+            int pivot = elements[(left + right) / 2];
+
+            while (i <= j)
+            {
+                while (elements[i].CompareTo(pivot) < 0)
+                {
+                    i++;
+                }
+                while (elements[j].CompareTo(pivot) > 0)
+                {
+                    j--;
+                }
+
+
+                if (i <= j)
+                {
+                    // Swap
+                    int tmp = elements[i];
+                    elements[i] = elements[j];
+                    elements[j] = tmp;
+
+                    i++;
+                    j--;
+                }
+            }
+            // Recursive calls
+            if (left < j)
+            {
+                Quicksort(elements, left, j);
+            }
+            if (i < right)
+            {
+                Quicksort(elements, i, right);
+            }
+        }
+
+        private void sfb_Click(object sender, RoutedEventArgs e)
+        {
+            Quicksort(inde, 0, inde.Length - 1);
+            foreach (var item in inde.ToList())
+            {
+                lia.Items.Add(item);
+            }
+        }
+
+        private void lia_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ tretiya/MainWindow.xaml.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tretiya
+{
+    public partial class MainWindow : Window
+    {
+        private static List<user> users = new List<user>();
+        //private static int[] inde = new int[5];
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void Load()
+        {
+            users.Add(new user
+            {
+                id = Convert.ToInt32(i.Text),
+                name = nam.Text,
+                password = passwor.Text,
+            });
+        }
+
+        public class user
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public string password { get; set; }
+        }
+
+        private void ba_Click(object sender, RoutedEventArgs e)
+        {
+            Load();
+            lia.Items.Clear();
+            foreach (var item in users)
+            {
+                lia.Items.Add(item);
+            }
+        }
+
+       
+        private void ShakeSort(List<user> arr)
+        {
+            int left = 0;
+            int right = arr.Count - 1;
+            bool swapped;
+
+            do
+            {
+                swapped = false;
+                for (int i = left; i < right; i++)
+                {
+                    if (arr[i].id > arr[i + 1].id)
+                    {
+                        Swap(arr, i, i + 1);
+                        swapped = true;
+                    }
+                }
+                right--;
+
+                if (swapped)
+                {
+                    swapped = false;
+                    for (int i = right; i > left; i--)
+                    {
+                        if (arr[i].id < arr[i - 1].id)
+                        {
+                            Swap(arr, i, i - 1);
+                            swapped = true;
+                        }
+                    }
+                    left++;
+                }
+            } while (swapped);
+        }
+
+
+        private void QuickSort(List<user> arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivotIndex = Partition(arr, left, right);
+                QuickSort(arr, left, pivotIndex - 1);
+                QuickSort(arr, pivotIndex + 1, right);
+            }
+        }
+
+
+        private int Partition(List<user> arr, int left, int right)
+        {
+            int pivot = arr[right].id;
+            int i = left - 1;
+
+            for (int j = left; j < right; j++)
+            {
+                if (arr[j].id <= pivot)
+                {
+                    i++;
+                    Swap(arr, i, j);
+                }
+            }
+            Swap(arr, i + 1, right);
+            return i + 1;
+        }
+
+        private void Swap(List<user> arr, int i, int j)
+        {
+            user temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        private void btn_SortShake_Click(object sender, RoutedEventArgs e)
+        {
+            ShakeSort(users);
+            lia.Items.Clear();
+            foreach (var item in users)
+            {
+                lia.Items.Add(item);
+            }
+        }
+
+        private void btn_SortQuick_Click(object sender, RoutedEventArgs e)
+        {
+            QuickSort(users, 0, users.Count - 1);
+            lia.Items.Clear();
+            foreach (var item in users)
+            {
+                lia.Items.Add(item);
+            }
+        }
+    }
+}
